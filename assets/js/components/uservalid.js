eec2db796744dead550b58d8085e6e60bef4c780
Steven Contreras <132318055+Jefsteven22@users.noreply.github.com>
2023-06-15 23:08:27 -0500

    se sube la prueba de ruta

--- assets/js/components/uservalid.js
+++ assets/js/components/uservalid.js
@@ -3,7 +3,7 @@ function uservalid () {
   const user = JSON.parse(localStorage.getItem('login_success')) || false
 
   if (!user) {
-    window.location.href = '/Talleres/E-commerce-proyecto-1/assets/html/login.html'
+    window.location.href = './../../html/login.html'
   }
 
 }
